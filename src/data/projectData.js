58a48058b3b7b42702dd3f56434ea20b7cb19f46
khakikokyo <phyeyoung90@gmail.com>
2022-12-22 05:16:44 +0900

    projectData 수정

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,33 +1,15 @@
 const projectData = [
   {
     id: 1,
-    title: "Portfolio Site",
-    skill: "#React, #HTML5 #CSS3",
-    contents: "포트폴리오 용도로 제작한 React 웹사이트입니다. 간단한 자기소개와 인적사항, 사용가능한 기술 스택과 진행한 프로젝트 경험들을 한눈에 확인하고, 소개하는 포트폴리오 사이트 입니다. Github 와 프로젝트 사이트로의 이동과 제 이력을 간편하게 확인하실 수 있습니다.",
-    imgUrl: "./img/project_main.png",
-    gitUrl: "https://github.com/khakikokyo/react-portfolio-site",
-    siteUrl: "https://phy-portfolio.netlify.app/"
+    title: "Netflix",
+    skill: "#React, #HTML5, #CSS3, #Styled-Components, #API, #AJAX-axios",
+    contents: "The Movie DB API를 활용한 현재 상영중인 영화의 정보를 장르별로 나열한 Netflix를 클론코딩한 Web Aplication Project 입니다. 영화 정보를 가져오는데 axios 외부 라이브러리를 사용하였고, 슬라이드 기능과 터치 슬라이드, 검색창 기능 등을 구현하였습니다.",
+    imgUrl: "./img/netflix_main.png",
+    gitUrl: "https://github.com/khakikokyo/react-netflix",
+    siteUrl: "https://khakikokyo.github.io/react-netflix/"
   },
   {
     id: 2,
-    title: "Starbucks",
-    skill: "#HTML5, #CSS3, #Java Script",
-    contents: "스타벅스 홈페이지를 클론코딩하여 제작하였습니다. HTML, CSS, JS를 활용하여 GooGle Fonts 와 Icons, 그리고 GSAP 의 ScrollToPlugin, Swiper 등과 같은 라이브러리를 사용하여 웹사이트를 제작하고 활용할 수 있는 기본적인 부분을 학습할 수 있었습니다.",
-    imgUrl: "./img/starbucks_main.png",
-    gitUrl: "https://github.com/khakikokyo/starbucks",
-    siteUrl: "https://phy-starbucks.netlify.app/"
-  },
-  {
-    id: 3,
-    title: "Overwatch",
-    skill: "#HTML5, #SCSS",
-    contents: "게임 overwatch의 영웅 선택 화면을 HTML과 SCSS로 구현하였습니다. HTML과 CSS를 사용하여 완성한 후에 SCSS로 리팩토링 하였습니다.",
-    imgUrl: "./img/overwatch_main.png",
-    gitUrl: "https://github.com/khakikokyo/overwatch",
-    siteUrl: "https://phy-overwatch.netlify.app/"
-  },
-  {
-    id: 4,
     title: "Todo List",
     skill: "#React, #HTML5, #CSS3, #LocalStorage",
     contents: "오늘 할 일을 적어놓고 관리해 주는 ToDay Todo List Project입니다. React 학습을 위해 글추가/삭제/전체삭제/할일완료/localStorage-데이터저장 등의 기능을 구현했습니다.",
@@ -36,13 +18,31 @@ const projectData = [
     siteUrl: "https://phy-todolist.netlify.app/"
   },
   {
+    id: 3,
+    title: "Starbucks",
+    skill: "#HTML5, #CSS3, #Java Script",
+    contents: "스타벅스 홈페이지를 클론코딩하여 제작하였습니다. HTML, CSS, JS를 활용하여 GooGle Fonts 와 Icons, 그리고 GSAP 의 ScrollToPlugin, Swiper 등과 같은 라이브러리를 사용하여 웹사이트를 제작하고 활용할 수 있는 기본적인 부분을 학습할 수 있었습니다.",
+    imgUrl: "./img/starbucks_main.png",
+    gitUrl: "https://github.com/khakikokyo/starbucks",
+    siteUrl: "https://phy-starbucks.netlify.app/"
+  },
+  {
+    id: 4,
+    title: "Overwatch",
+    skill: "#HTML5, #SCSS",
+    contents: "게임 overwatch의 영웅 선택 화면을 HTML과 SCSS로 구현하였습니다. HTML과 CSS를 사용하여 완성한 후에 SCSS로 리팩토링 하였습니다.",
+    imgUrl: "./img/overwatch_main.png",
+    gitUrl: "https://github.com/khakikokyo/overwatch",
+    siteUrl: "https://phy-overwatch.netlify.app/"
+  },
+  {
     id: 5,
-    title: "Netflix",
-    skill: "#React, #HTML5, #CSS3, #Styled-Components, #API, #AJAX-axios",
-    contents: "The Movie DB API를 활용한 현재 상영중인 영화의 정보를 장르별로 나열한 Netflix를 클론코딩한 Web Aplication Project 입니다. 영화 정보를 가져오는데 axios 외부 라이브러리를 사용하였고, 슬라이드 기능과 터치 슬라이드, 검색창 기능 등을 구현하였습니다.",
-    imgUrl: "./img/netflix_main.png",
-    gitUrl: "https://github.com/khakikokyo/react-netflix",
-    siteUrl: "https://khakikokyo.github.io/react-netflix/"
+    title: "Portfolio Site",
+    skill: "#React, #HTML5 #CSS3",
+    contents: "포트폴리오 용도로 제작한 React 웹사이트입니다. 간단한 자기소개와 인적사항, 사용가능한 기술 스택과 진행한 프로젝트 경험들을 한눈에 확인하고, 소개하는 포트폴리오 사이트 입니다. Github 와 프로젝트 사이트로의 이동과 제 이력을 간편하게 확인하실 수 있습니다.",
+    imgUrl: "./img/project_main.png",
+    gitUrl: "https://github.com/khakikokyo/react-portfolio-site",
+    siteUrl: "https://phy-portfolio.netlify.app/"
   },
   {
     id: 6,
