0900536489732d2f0dd3a755964864bb97ec0ecc
khakikokyo <phyeyoung90@gmail.com>
2022-12-22 05:11:41 +0900

    project Netflix Project 추가, 준비중 페이지 추가

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -29,11 +29,29 @@ const projectData = [
   {
     id: 4,
     title: "Todo List",
-    skill: "#React, #HTML5, #css3",
+    skill: "#React, #HTML5, #CSS3, #LocalStorage",
     contents: "오늘 할 일을 적어놓고 관리해 주는 ToDay Todo List Project입니다. React 학습을 위해 글추가/삭제/전체삭제/할일완료/localStorage-데이터저장 등의 기능을 구현했습니다.",
     imgUrl: "./img/todolist_main.png",
     gitUrl: "https://github.com/khakikokyo/todo-list",
     siteUrl: "https://phy-todolist.netlify.app/"
+  },
+  {
+    id: 5,
+    title: "Netflix",
+    skill: "#React, #HTML5, #CSS3, #Styled-Components, #API, #AJAX-axios",
+    contents: "The Movie DB API를 활용한 현재 상영중인 영화의 정보를 장르별로 나열한 Netflix를 클론코딩한 Web Aplication Project 입니다. 영화 정보를 가져오는데 axios 외부 라이브러리를 사용하였고, 슬라이드 기능과 터치 슬라이드, 검색창 기능 등을 구현하였습니다.",
+    imgUrl: "./img/netflix_main.png",
+    gitUrl: "https://github.com/khakikokyo/react-netflix",
+    siteUrl: "https://khakikokyo.github.io/react-netflix/"
+  },
+  {
+    id: 6,
+    title: "Project 준비중...",
+    skill: "",
+    contents: "현재 Project 준비중입니다.",
+    imgUrl: "./img/ready_pc.jpg",
+    gitUrl: "",
+    siteUrl: ""
   }
 ];
 
