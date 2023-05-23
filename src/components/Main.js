79655a240f66286a23cec71950565eb3757e5882
khakikokyo <phyeyoung90@gmail.com>
2023-05-23 19:29:55 +0900

    Main-Portfolio Site 이미지 변경

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ function Main() {
   return(
     <div className="main__bg">
       <div className="main__text1">안녕하세요.</div>
-      <div className="main__text2 display-enter"><TypingText text={"박혜영 입니다. \n 제 포트폴리오 사이트에 와주셔서 감사합니다."} /></div>
+      <div className="main__text2 display-enter"><TypingText text={"방문해 주셔서 감사합니다. \n 저는 박혜영 입니다."} /></div>
     </div>
   )
 }
